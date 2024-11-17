f0673102d2bdb5cb6e905178820690335c2df734
Remil <51848204+RemilRLs@users.noreply.github.com>
2024-11-17 17:43:35 +0100

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -19,26 +19,22 @@ byte_list = bytes.fromhex(hex_str)
 print(byte_list.decode('ascii'))
 
 # Encoding: BASE64
-# Cha?ne hexad?cimale
 hex_str = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
 
-# Convertir la cha?ne hexad?cimale en bytes
+# Encoding : HEX
+
 byte_data = bytes.fromhex(hex_str)
 
-# Encoder en Base64
 base64_encoded = base64.b64encode(byte_data)
 
-# Convertir en cha?ne ASCII pour affichage
 ascii_result = base64_encoded.decode('ascii')
 
 print(ascii_result.replace('+', '_'))
 
-# Encoding : Bytes and big integers
 
 n = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
 decoded_bytes = long_to_bytes(n)
 
-# Convertir les bytes en cha?ne ASCII
 decoded_message = decoded_bytes.decode('ascii')
 
 print(decoded_message)
